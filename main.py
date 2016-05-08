ce93390487d8820f773380c28aabf05b156446a5
Michiel <git@fastfox.nl>
2016-05-08 20:30:28 +0200

    BC110

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def go():
         # Login to BC
         login(s)
 
-        bc_number = '109'
+        bc_number = '110'
 
         # Load orders from WTOS
         orders = load_orders(bc_number)
