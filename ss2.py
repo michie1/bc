fa352b1b5c157ab62c208c190650f9f1bd7605f5
Michiel <git@fastfox.nl>
2016-10-13 20:33:18 +0200

    When no items, do not put in spreadsheet

--- ss2.py
+++ ss2.py
@@ -73,102 +73,58 @@ def add_to_spreadsheet(wks, orders):
     #for user, products in orders.iteritems():
     for user, products in sorted(orders.items()):
 
-        #column_number = 0
-        #cell_list[row_number*10].value = user.decode('utf-8')
-        cell_list[row_number*10].value = user # no decode ni python 3.5
-        row_number += 1
-        first_row = row_number
-
-        for product in products:
-            if product == None:
-                continue
-
-            try:
-                #cell_list[row_number*10+0].value, cell_list[row_number*10+1].value = product['name'].decode('utf-8').split(' ', 1) # no decode in python3.5
-                cell_list[row_number*10+0].value, cell_list[row_number*10+1].value = product['name'].split(' ', 1)
-            except IndexError as e:
-                print(e)
-                print(user, product)
-                print(product['type'])
-                print(row_number)
-                continue
-            except KeyError as e:
-                print(e)
-                print(user, product)
-                continue
-
-            cell_list[row_number*10+2].value = product['type']
-            cell_list[row_number*10+3].value = "=(\"" + product['sku'] + "\")"
-            cell_list[row_number*10+4].value = product['pa']
-            
-            # todo ceiling
-            cell_list[row_number*10+5].value = product['original_price']
-            cell_list[row_number*10+6].value = product['price']
-
-            cell_list[row_number*10+7].value = product['qty']
-            cell_list[row_number*10+8].value = "=G" + str(row_number+1) + "*H" + str(row_number+1)
-            cell_list[row_number*10+9].value = "=CEILING(I" + str(row_number+1) + "*0.95, 0.01)"
+        if len(products) > 0:
+            cell_list[row_number*10].value = user # no decode ni python 3.5
             row_number += 1
+            first_row = row_number
 
-        last_row = row_number - 1
+            for product in products:
+                if product == None:
+                    continue
 
-        cell_list[row_number*10+8].value = '=SUM(I' + str(first_row+1) + ':I' + str(last_row+1) + ')'
-        cell_list[row_number*10+9].value = '=SUM(J' + str(first_row+1) + ':J' + str(last_row+1) + ')'
+                try:
+                    #cell_list[row_number*10+0].value, cell_list[row_number*10+1].value = product['name'].decode('utf-8').split(' ', 1) # no decode in python3.5
+                    cell_list[row_number*10+0].value, cell_list[row_number*10+1].value = product['name'].split(' ', 1)
+                except IndexError as e:
+                    print(e)
+                    print(user, product)
+                    print(product['type'])
+                    print(row_number)
+                    continue
+                except KeyError as e:
+                    print(e)
+                    print(user, product)
+                    continue
 
-        #try:
-            #cell_list[row_number+8].value = '=SUM(I' + str(first_row) + ':I' + str(last_row) + ')'
-            #cell_list[row_number+9].value = '=SUM(J' + str(first_row) + ':J' + str(last_row) + ')'
-        #except gspread.exceptions.ConnectionError as err:
-            #print err
-            #memcache.set("busy", "0")
+                cell_list[row_number*10+2].value = product['type']
+                cell_list[row_number*10+3].value = "=(\"" + product['sku'] + "\")"
+                cell_list[row_number*10+4].value = product['pa']
+                
+                # todo ceiling
+                cell_list[row_number*10+5].value = product['original_price']
+                cell_list[row_number*10+6].value = product['price']
 
-        row_number += 2
-        print('Order ' + user + ' prepared for spreadsheet')
-        #time.sleep(1)
+                cell_list[row_number*10+7].value = product['qty']
+                cell_list[row_number*10+8].value = "=G" + str(row_number+1) + "*H" + str(row_number+1)
+                cell_list[row_number*10+9].value = "=CEILING(I" + str(row_number+1) + "*0.95, 0.01)"
+                row_number += 1
+
+            last_row = row_number - 1
+
+            cell_list[row_number*10+8].value = '=SUM(I' + str(first_row+1) + ':I' + str(last_row+1) + ')'
+            cell_list[row_number*10+9].value = '=SUM(J' + str(first_row+1) + ':J' + str(last_row+1) + ')'
+
+            #try:
+                #cell_list[row_number+8].value = '=SUM(I' + str(first_row) + ':I' + str(last_row) + ')'
+                #cell_list[row_number+9].value = '=SUM(J' + str(first_row) + ':J' + str(last_row) + ')'
+            #except gspread.exceptions.ConnectionError as err:
+                #print err
+                #memcache.set("busy", "0")
+
+            row_number += 2
+            print('Order ' + user + ' prepared for spreadsheet')
+            #time.sleep(1)
 
     wks.update_cells(cell_list)
 
     print('Orders added to spreadsheet')
-
-    #wks.update_cells(cell_list)
-
-    """
-    row_number = 2
-
-    for user, products in orders.iteritems():
-        wks.update_cell(row_number, 2, user.decode('utf-8'))
-        row_number += 1
-        first_row = row_number
-        for product in products:
-            print product
-            row = wks.range('A' + str(row_number) + ':J' + str(row_number))
-            row[0].value, row[1].value = product['name'].decode('utf-8').split(' ', 1)
-            row[2].value = product['type']
-            row[3].value = "=(\"" + product['sku'] + "\")"
-            row[4].value = product['pa']
-            row[5].value = product['original_price']
-            row[6].value = product['price']
-            row[7].value = product['qty']
-            row[8].value = "=G" + str(row_number) + "*H" + str(row_number)
-            row[9].value = "=CEILING(I" + str(row_number) + "*0.95, 0.01)"
-            #row[8].value = float(product['price']) * product['qty']
-            #row[9].value = math.ceil(float(row[5].value) * 0.95 * 100) / 100
-
-            wks.update_cells(row)
-            row_number += 1
-        last_row = row_number - 1
-        try:
-            wks.update_cell(row_number, 9, '=SUM(I' + str(first_row) + ':I' + str(last_row) + ')')
-            #wks.update_cell(row_number, 10, '=CEILING(I' + str(row_number) + '*0.95, 0.01)')
-            wks.update_cell(row_number, 10, '=SUM(J' + str(first_row) + ':J' + str(last_row) + ')')
-        except gspread.exceptions.ConnectionError as err:
-            print err
-            memcache.set("busy", "0")
-
-        row_number += 2
-        print 'Order ' + user + ' in spreadsheet'
-        time.sleep(1)
-
-    print 'Orders added to spreadsheet'
-    """
-
