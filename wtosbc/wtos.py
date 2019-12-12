2cbbc3aca722011cd63c162a40bc3ba50bcf3192
Michiel <git@fastfox.nl>
2019-12-12 19:58:37 +0100

    Refactor get_post_items_per_user

--- wtosbc/wtos.py
+++ wtosbc/wtos.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 import requests
 import lxml.html
 import xmltodict
 from urllib.request import urlopen
 import json
 import ssl
-from typing import cast
+from typing import cast, Tuple
 
 from wtosbc import config, spreadsheet
 from wtosbc.custom_types import *
@@ -65,12 +64,14 @@ def get_post_items_per_user(bc_number: int, posts: Posts) -> PostItemsPerUser:
         user = post["display_name"]
 
         if content[2:11] == str(int(bc_number) + 1) + " start":  # next start
+            # TODO: Check if you can do this outside/before this function
             if user == bc_chef:
-                increment_bc_number(int(bc_number) + 1)
+                increment_bc_number(bc_number + 1)
                 reset_state_pa()
-                create_next_sheet(int(bc_number) + 1)
+                create_next_sheet(bc_number + 1)
                 break
-        elif content[2:11] == str(int(bc_number) + 1) + " PA":  # next start
+        elif content[2:11] == str(bc_number + 1) + " PA":  # next start
+            # TODO: Check if you can do this outside/before this function
             if user == bc_chef:
                 set_state_pa()
                 break
@@ -86,71 +87,80 @@ def get_post_items_per_user(bc_number: int, posts: Posts) -> PostItemsPerUser:
                     .replace("<strong>", "[b]")
                     .replace("</strong>", "[/b]")
                 )
-                if line != "":
-                    if line == "---" or line == "--" or line == "—" or line == "&nbsp;":
-                        break
-                    elif line[0] == "-" and line[-1] == "-":
-                        continue
-                    elif line[0:2] == "<a":
-                        line = "1x " + line
-                    if line[0:5] == "<del>":
-                        continue
-                    elif line == "WTOS":
-                        if user == bc_chef:
-                            user = "WTOS"
-                            post_items_per_user[user] = []
-                        else:
-                            break
+
+                if (
+                    line == ""
+                    or line == "&nbsp;"
+                    or (line[0] == "-" and line[-1] == "-")
+                ):
+                    continue
+
+                if line == "---" or line == "--" or line == "—":
+                    break
+
+                if line == "WTOS":
+                    if user == bc_chef:
+                        user = "WTOS"
+                        post_items_per_user[user] = []
+                    break
+
+                try:
+                    product_qty_str, product = line.split("x ", 1)
+                    product_qty = int(product_qty_str.strip())
+                except ValueError as e:
+                    print("ValueError")
+                    print(e)
+                    print(line)
+                    continue
+
+                if product_qty > 0:
+                    (product_url, type_pa) = split_url_type_pa(product)
+
+                    if "bike-components.de" in product_url:
+                        (product_type, product_pa) = split_type_pa(type_pa)
+
+                        # fix for " in type
+                        product_type = (
+                            product_type.replace("&quot;", '"')
+                            .replace("&nbsp;", "")
+                            .strip()
+                        )
+
+                        post_items_per_user[user].append(
+                            {
+                                "url": product_url,
+                                "type": product_type,
+                                "qty": product_qty,
+                                "pa": product_pa,
+                            }
+                        )
                     else:
-                        try:
-                            product_qty_str, product = line.split("x ", 1)
-                            product_qty = int(product_qty_str.strip())
-                        except ValueError as e:
-                            print("ValueError")
-                            print(e)
-                            print(line)
-                            continue
-
-                        if product_qty > 0:
-                            splitted = product.split(" ")
-                            product_url = splitted[0]
-
-                            if "bike-components.de" in product_url:
-                                # Divide type and pa
-                                type_pa = " ".join(splitted[1:])
-                                strong = type_pa.split("[b]")
-
-                                # type and PA exists
-                                if len(strong) == 2:
-                                    product_type = strong[0].strip()
-                                    product_pa = strong[1].replace("[/b]", "").strip()
-                                else:
-                                    # type or PA exist
-                                    if type_pa[0:8] == "<strong>":
-                                        # PA
-                                        product_pa = type_pa[8:-9]
-                                        product_type = ""
-                                    else:
-                                        # type
-                                        product_type = type_pa
-                                        product_pa = ""
-
-                                # fix for " in type
-                                product_type = (
-                                    product_type.replace("&quot;", '"')
-                                    .replace("&nbsp;", "")
-                                    .strip()
-                                )
-
-                                post_items_per_user[user].append(
-                                    {
-                                        "url": product_url,
-                                        "type": product_type,
-                                        "qty": product_qty,
-                                        "pa": product_pa,
-                                    }
-                                )
-                            else:
-                                print("Wrong url: ", product_url)
+                        print("Wrong url: ", product_url)
 
     return post_items_per_user
+
+
+def split_url_type_pa(product: str) -> Tuple[str, str]:
+    splitted = product.split(" ")
+    product_url = splitted[0]
+    type_pa = " ".join(splitted[1:])
+    return (product_url, type_pa)
+
+
+def split_type_pa(type_pa: str) -> Tuple[str, str]:
+    strong = type_pa.split("[b]")
+
+    if len(strong) == 2:
+        # type and PA exists
+        product_type = strong[0].strip()
+        product_pa = strong[1].replace("[/b]", "").strip()
+    else:
+        # type or PA
+        if type_pa[0:8] == "<strong>":
+            product_pa = type_pa[8:-9]
+            product_type = ""
+        else:
+            product_type = type_pa
+            product_pa = ""
+
+    return (product_type, product_pa)
