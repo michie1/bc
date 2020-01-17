0b0dd1a3950e19e559816d040e089697b52c2bb2
Michiel <git@fastfox.nl>
2020-01-17 19:05:21 +0100

    Join multiple items together and do not post them separately

--- tests/test_bc.py
+++ tests/test_bc.py
@@ -2,8 +2,9 @@ import json
 from snapshottest import assert_match_snapshot, Snapshot
 from typing import cast, Any
 import lxml.html
-from wtosbc.custom_types import PostItem
+from wtosbc.custom_types import PostItem, OrderItemsPerUser, Payloads
 from wtosbc import bc
+import pytest
 
 Snapshot = Any
 
@@ -21,6 +22,28 @@ def test_get_product(snapshot: Snapshot) -> None:
     snapshot.assert_match(product)
 
 
+def test_combine_order_items() -> None:
+    order_items_per_user = load_test_order_items_per_user()
+    order_items = bc.get_payloads(order_items_per_user)
+    expected_order_items: Payloads = [
+        {
+            "id": "46718",
+            "quantity": 4,
+            "token": "44b3daca3f1958cd7dc7c1eb511beef68764769f",
+            "type_id": "200241",
+        }
+    ]
+
+    assert order_items == expected_order_items
+
+
 def load_test_product_page_html(url: str) -> str:
     with open(url) as product_page:
         return product_page.read()
+
+
+def load_test_order_items_per_user() -> OrderItemsPerUser:
+    with open(
+        "tests/order_items_per_user_double_order_items.json"
+    ) as order_items_per_user:
+        return cast(OrderItemsPerUser, json.loads(order_items_per_user.read()))
